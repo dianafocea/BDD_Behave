d76302f6e1eaf32c35d5026a4a8ff62e57d97a4d
dianafocea <dianafocea13>
2022-12-02 13:56:06 +0200

    +

--- pages/home_page.py
+++ pages/home_page.py
@@ -11,10 +11,10 @@ class HomePage(Browser):
     FORM_AUTH_LINK = (By.LINK_TEXT, "Form Authentication")
     FORGOT_PASSWORD_LINK = (By.LINK_TEXT, "Forgot Password")
     BASIC_AUTH_LINK = (By.LINK_TEXT, "Basic Auth")
-
+    HOME_PAGE_URL = 'https://the-internet.herokuapp.com'
 
     def navigate_to_home_page(self):
-        self.driver.get("https://the-internet.herokuapp.com")
+        self.driver.get(*self.HOME_PAGE_URL)
 
     def click_form_auth(self):
         self.driver.find_element(*self.FORM_AUTH_LINK).click()
